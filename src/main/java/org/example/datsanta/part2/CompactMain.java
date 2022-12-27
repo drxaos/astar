784e9ad7823e43c2347a503016be594aeeae40b2
ivanvasilevskij <i.vaievskiy@dats.team>
2022-12-27 12:09:23 +0300

    add CompactMain

--- src/main/java/org/example/datsanta/part2/CompactMain.java
+++ src/main/java/org/example/datsanta/part2/CompactMain.java
@@ -131,29 +131,29 @@ public class CompactMain {
             };
         }).toList();
 
-        Map<ChildType, List<GiftType>> best = new HashMap<>();
-        best.put(new ChildType(male, 0), List.of(constructors));
-        best.put(new ChildType(male, 1), List.of(dolls));
-        best.put(new ChildType(male, 2), List.of(radio_controlled_toys));
-        best.put(new ChildType(male, 3), List.of(toy_vehicles));
-        best.put(new ChildType(male, 4), List.of(board_games));
-        best.put(new ChildType(male, 5), List.of(outdoor_games));
-        best.put(new ChildType(male, 6), List.of(playground));
-        best.put(new ChildType(male, 7), List.of(soft_toys));
-        best.put(new ChildType(male, 8), List.of(computer_games));
-        best.put(new ChildType(male, 9), List.of(sweets));
-        best.put(new ChildType(male, 10), List.of(books));
-        best.put(new ChildType(female, 0), List.of(constructors));
-        best.put(new ChildType(female, 1), List.of(dolls));
-        best.put(new ChildType(female, 2), List.of(radio_controlled_toys));
-        best.put(new ChildType(female, 3), List.of(toy_vehicles));
-        best.put(new ChildType(female, 4), List.of(board_games));
-        best.put(new ChildType(female, 5), List.of(outdoor_games));
-        best.put(new ChildType(female, 6), List.of(playground));
-        best.put(new ChildType(female, 7), List.of(soft_toys));
-        best.put(new ChildType(female, 8), List.of(computer_games));
-        best.put(new ChildType(female, 9), List.of(pet));
-        best.put(new ChildType(female, 10), List.of(clothes));
+        Map<ChildType, Map<GiftType, Double>> best = new HashMap<>();
+        best.put(new ChildType(male, 0), Map.of(toy_vehicles, 0.130));
+        best.put(new ChildType(male, 1), Map.of(sweets, 0.441));
+        best.put(new ChildType(male, 2), Map.of(radio_controlled_toys, 0.408));
+        best.put(new ChildType(male, 3), Map.of(sweets, 0.578));
+        best.put(new ChildType(male, 4), Map.of(computer_games, 0.602));
+        best.put(new ChildType(male, 5), Map.of(computer_games, 0.798));
+        best.put(new ChildType(male, 6), Map.of(computer_games, 0.803));
+        best.put(new ChildType(male, 7), Map.of(computer_games, 0.8));
+        best.put(new ChildType(male, 8), Map.of(computer_games, 0.804));
+        best.put(new ChildType(male, 9), Map.of(computer_games, 0.803));
+        best.put(new ChildType(male, 10), Map.of(computer_games, 0.813));
+        best.put(new ChildType(female, 0), Map.of(playground, 0.065));
+        best.put(new ChildType(female, 1), Map.of(sweets, 0.434));
+        best.put(new ChildType(female, 2), Map.of(sweets, 0.439));
+        best.put(new ChildType(female, 3), Map.of(outdoor_games, 0.453));
+        best.put(new ChildType(female, 4), Map.of(outdoor_games, 0.463));
+        best.put(new ChildType(female, 5), Map.of(sweets, 0.571));
+        best.put(new ChildType(female, 6), Map.of(dolls, 0.531));
+        best.put(new ChildType(female, 7), Map.of(dolls, 0.531));
+        best.put(new ChildType(female, 8), Map.of(books, 0.454));
+        best.put(new ChildType(female, 9), Map.of(books, 0.45));
+        best.put(new ChildType(female, 10), Map.of(pet, 0.246));
 
 
         int max = 100000;
@@ -161,7 +161,17 @@ public class CompactMain {
         final List<Gift2> unmodifiedGifts = new ArrayList<>(step2Map.getGifts());
 
         final List<Gift2> gifts = step2Map.getGifts();
-        final List<Child2> children = step2Map.getChildren();
+        final List<Child2> children = new ArrayList<>(
+                step2Map.getChildren()
+                        .stream()
+                        .sorted(Comparator.comparing(e -> best.get(new ChildType(e.getGender(), e.getAge()))
+                                .values()
+                                .stream()
+                                .mapToDouble(w -> w)
+                                .max()
+                                .getAsDouble() * -1)
+                        ).toList()
+        );
         final List<Presenting> result1 = new ArrayList<>();
         final List<Presenting> result2 = new ArrayList<>();
 
@@ -169,11 +179,15 @@ public class CompactMain {
         while (!children.isEmpty()) {
             final Child2 child2 = children.remove(0);
             final ChildType childType = new ChildType(child2.getGender(), child2.getAge());
-            final List<GiftType> giftTypes = best.get(childType);
+            final List<Map.Entry<GiftType, Double>> entries = best.get(childType)
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .toList();
 
             final Gift2 gift = isFirstFull
-                    ? getMinPriceGift(gifts, giftTypes)
-                    : getMaxPriceGift(gifts, giftTypes);
+                    ? getMinPriceGift(gifts, entries)
+                    : getMaxPriceGiftWithEntries(gifts, entries);
             gifts.remove(gift);
 
             if (sum + gift.getPrice() > 100000) {
@@ -207,14 +221,40 @@ public class CompactMain {
     private static Gift2 getMaxPriceGift(final List<Gift2> gifts, final List<GiftType> giftTypes) {
         return gifts.stream()
                 .filter(e -> giftTypes.contains(e.getType()))
+//                .sorted(Comparator.comparing(Gift2::getPrice).thenComparing(e -> e.g))
                 .max(Comparator.comparing(Gift2::getPrice))
                 .get();
     }
 
-    private static Gift2 getMinPriceGift(final List<Gift2> gifts, final List<GiftType> giftTypes) {
-        return gifts.stream()
-                .filter(e -> giftTypes.contains(e.getType()))
-                .min(Comparator.comparing(Gift2::getPrice))
+    private static Gift2 getMaxPriceGiftWithEntries(final List<Gift2> gifts, final List<Map.Entry<GiftType, Double>> giftTypes) {
+        final Gift2 gift2 = gifts.stream()
+                .filter(e -> giftTypes.stream().map(Map.Entry::getKey).toList().contains(e.getType()))
+                .max(
+                        Comparator.comparing(e -> giftTypes.stream()
+                                .filter(r -> r.getKey() == e.getType())
+                                .findFirst()
+                                .map(Map.Entry::getValue)
+                                .get() * e.getPrice()
+                        ))
                 .get();
+
+        return gift2;
+    }
+
+    private static Gift2 getMinPriceGift(final List<Gift2> gifts, List<Map.Entry<GiftType, Double>> giftTypes) {
+        final Gift2 gift2 = gifts.stream()
+                .filter(e -> giftTypes.stream().map(Map.Entry::getKey).toList().contains(e.getType()))
+                .sorted(
+                        Comparator.comparing(e -> giftTypes.stream()
+                                .filter(r -> r.getKey() == e.getType())
+                                .findFirst()
+                                .map(Map.Entry::getValue)
+                                .get()
+                        )
+                )
+                .findFirst()
+                .get();
+
+        return gift2;
     }
 }
